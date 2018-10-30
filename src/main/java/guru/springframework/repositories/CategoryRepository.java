50cfba1e7bbc0709f9d0c08bf4839eb2efacd965
Elhadji Barry <elhadji.a.barry@desjardins.com>
2018-10-30 19:01:28 -0400

    8.147 Spring data JPA query methods

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -1,8 +1,11 @@
 package guru.springframework.repositories;
 
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import guru.springframework.domain.Category;
 import guru.springframework.domain.Recipe;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String desciption);
 }
