aeb0bcd48ad19e80687d2ddecb041184f75c1377
Elhadji Barry <elhadji.a.barry@desjardins.com>
2018-10-30 19:45:29 -0400

    8.149 Assignment Review: display list of recipes

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -19,6 +19,12 @@ public class Ingredient {
     @OneToOne(fetch = FetchType.EAGER)
     private UnitOfMeasure unitOfMeasure;
 
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+        this.description = description;
+        this.amount = amount;
+        this.unitOfMeasure = uom;
+        this.recipe = recipe;
+    }
 
     public Long getId() {
         return id;
