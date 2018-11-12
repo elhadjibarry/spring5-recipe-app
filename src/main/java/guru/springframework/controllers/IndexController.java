b3a66458b4b7998151997ca2f0bf2bb2a4e75bee
elhadji.barry <elhadji.barry@gmail.com>
2018-11-11 21:14:43 -0500

    Project Lombok refactoring

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,16 +1,13 @@
 package guru.springframework.controllers;
 
-import java.util.Optional;
+import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import guru.springframework.domain.Category;
-import guru.springframework.domain.UnitOfMeasure;
-import guru.springframework.repositories.CategoryRepository;
-import guru.springframework.repositories.UnitOfMeasureRepository;
-import guru.springframework.services.RecipeService;
 
 @Controller
+@Slf4j
 public class IndexController {
 
     private RecipeService recipeService;
@@ -21,6 +18,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
+        log.debug("Loading index page");
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -1,12 +1,15 @@
 package guru.springframework.services;
 
-import java.util.HashSet;
-import java.util.Set;
-import org.springframework.stereotype.Service;
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
+@Slf4j
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
@@ -17,6 +20,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("Service - get the recipes");
         Set<Recipe> recipes = new HashSet<>();
         recipeRepository.findAll().forEach(recipes::add);
         return recipes;
