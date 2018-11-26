61b369f4389dfad219c413962ff3fe69b5632324
elhadji.barry <elhadji.barry@gmail.com>
2018-11-25 21:19:31 -0500

    11.208 View ingredients

---
+++ src/main/java/guru/springframework/controllers/IngredientController.java
@@ -0,0 +1,28 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class IngredientController {
+
+    private RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping
+    @RequestMapping("/recipe/{recipeId}/ingredients")
+    public String listIngredients(Model model, @PathVariable String recipeId) {
+        log.debug("Loading ingredients for recipe " + recipeId);
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+        return "recipe/ingredient/list";
+    }
+}
